403a5ee38a0274ca8bc2ba61d913ff1a0cde565a
Khalentello <nicofelipe26@hotmail.com>
2023-01-05 18:37:28 -0500

    style changing

--- src/index.js
+++ src/index.js
@@ -91,21 +91,23 @@ class Game extends React.Component {
     });
     let status;
     if (winner) {
-      status = "Winner:" + winner;
+      status = "Winner: " + winner;
     } else {
       status = "Next plater: " + (this.state.xIsNext ? "X" : "O");
     }
     return (
-      <div className="game">
-        <div className="game-board">
-          <Board
-            squares={current.squares}
-            onClick={(i) => this.handleClick(i)}
-          />
-        </div>
-        <div className="game-info">
-          <div>{status}</div>
-          <ol>{moves}</ol>
+      <div className="mainContainer">
+        <div className="game">
+          <div className="game-board">
+            <div>{status}</div>
+            <Board
+              squares={current.squares}
+              onClick={(i) => this.handleClick(i)}
+            />
+          </div>
+          <div className="game-info">
+            <ol>{moves}</ol>
+          </div>
         </div>
       </div>
     );
